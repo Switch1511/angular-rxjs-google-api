6cb5524fc96104593eb8dd55d39682a17cd7151b
Switch1511 <gustavopasqualli0@gmail.com>
2023-04-19 00:40:55 -0300

    first commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-rxjs-google-api'),
+      dir: require('path').join(__dirname, './coverage/buscante'),
       subdir: '.',
       reporters: [
         { type: 'html' },
---
+++ src/app/service/livro.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+
+  private readonly API: string = "AIzaSyBwqzpTuihGpK2Ou3SGn7yXuG8oW5dW-Hk"
+
+  constructor() { }
+
+
+}
